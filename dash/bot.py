58d8bb79a8693d9ce84a1dc81f680b9e9542b872
Tomodachi94 <tomodachi94@protonmail.com>
2021-08-30 16:37:42 -0700

    Initialize bot - Create folder `dash/` - Create `bot.py`     - Add command `about`

---
+++ dash/bot.py
@@ -0,0 +1,21 @@
+import lightbulb
+import dotenv
+import os
+import logging
+
+# environment
+dotenv.load_dotenv()
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+DISCORD_PREFIX = os.environ.get("DISCORD_PREFIX", "_")
+
+bot = lightbulb.Bot(token=DISCORD_TOKEN, prefix=DISCORD_PREFIX, logs="INFO")
+
+@bot.commands
+async def about(ctx):
+    await ctx.respond(
+    """
+    Dash is a bot for the FTB Wiki's Discord.
+    https://github.com/Tomodachi94/dash2
+    """)
+
+bot.run()
