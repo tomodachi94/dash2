c463746321ff197991e6a180eb6bdabda7c8b1c3
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-05-27 17:51:37 -0700

    Fix extension path bug
    Dash's driver code `dash/main.py` was attempting to call `./ext/`, and it broke
    on Heroku.
    
    - https://www.delftstack.com/howto/python/python-get-path/
    - https://www.geeksforgeeks.org/python-os-path-join-method/
    
    I used some `os.path` hackery to get it working again.

--- dash/main.py
+++ dash/main.py
@@ -1,5 +1,6 @@
 import dotenv
 import os
+import pathlib
 import json
 
 import hikari
@@ -11,8 +12,11 @@ DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 DISCORD_PREFIX = os.environ.get("DISCORD_PREFIX", "_")
 GIPHY_TOKEN = os.environ["GIPHY_TOKEN"]
 
+DASH_PATH = os.path.abspath(os.getcwd())
+DASH_EXT_PATH = os.path.join(os.path.abspath(os.getcwd()), "ext")
+
 bot = lightbulb.BotApp(token=DISCORD_TOKEN, prefix=DISCORD_PREFIX, logs="INFO")
 
-bot.load_extensions_from("./ext/")
+bot.load_extensions_from(DASH_EXT_PATH)
 
 bot.run()
