62c26d39a03fa7f84f79078c513c11d84947145f
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-05-27 19:08:52 -0700

    Patch Heroku os.getcwd bug

--- dash/main.py
+++ dash/main.py
@@ -12,8 +12,11 @@ DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 DISCORD_PREFIX = os.environ.get("DISCORD_PREFIX", "_")
 GIPHY_TOKEN = os.environ["GIPHY_TOKEN"]
 
-DASH_PATH = os.path.abspath(os.getcwd())
-DASH_EXT_PATH = os.path.join(os.path.abspath(os.getcwd()), "ext")
+DASH_PATH = os.path.realpath(__file__)
+DASH_PATH = os.path.dirname(DASH_PATH)
+DASH_EXT_PATH = os.path.join(DASH_PATH, "ext")
+print("Dash path: ", DASH_PATH)
+print("Dash ext path: ", DASH_EXT_PATH)
 
 bot = lightbulb.BotApp(token=DISCORD_TOKEN, prefix=DISCORD_PREFIX, logs="INFO")
 
