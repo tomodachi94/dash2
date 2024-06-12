8e93bc0d2479b63cbd2c983e43b7b5ff8c3ad616
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-11 23:10:21 -0700

    chore: fix linter warnings

--- dash/__main__.py
+++ dash/__main__.py
@@ -13,7 +13,6 @@ def main():
     dotenv.load_dotenv()
     DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
     DISCORD_PREFIX = os.environ.get("DISCORD_PREFIX", "_")
-    GIPHY_TOKEN = os.environ["GIPHY_TOKEN"]
 
     DASH_PATH = os.path.realpath(__file__)
     DASH_PATH = os.path.dirname(DASH_PATH)
