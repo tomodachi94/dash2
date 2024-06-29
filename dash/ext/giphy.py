9b987517405b42f12ae74840cc9e84ec902ec511
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-29 11:44:28 -0700

    Migrate to Lightbulb v3 (ext/giphy.py)

--- dash/ext/giphy.py
+++ dash/ext/giphy.py
@@ -4,31 +4,22 @@ import json
 import lightbulb
 from ezgiphy import GiphyPublicAPI
 
-plugin = lightbulb.Plugin("Giphy")
+loader = lightbulb.Loader()
 
 GIPHY_TOKEN = os.environ["GIPHY_TOKEN"]
 
 
-@plugin.command()
-@lightbulb.command("bunny", "Sends a bunny GIF.")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def bunny(ctx: lightbulb.Context):
-    """
-    Sends a bunny GIF using the giphy API.
-    """
-    giphy = GiphyPublicAPI(GIPHY_TOKEN)
-    tag = "rabbit"
+@loader.command
+class BunnyCommand(
+    lightbulb.SlashCommand, name="bunny", description="Sends a bunny GIF."
+):
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        giphy = GiphyPublicAPI(GIPHY_TOKEN)
+        tag = "rabbit"
 
-    data = giphy.random(tag=tag, rating="g", limit=1)
-    data = json.loads(data)
-    data = data["data"]
-    embed_url = data["embed_url"]
-    await ctx.respond(embed_url)
-
-
-def load(bot):
-    bot.add_plugin(plugin)
-
-
-def unload(bot):
-    bot.remove_plugin(plugin)
+        data = giphy.random(tag=tag, rating="g", limit=1)
+        data = json.loads(data)
+        data = data["data"]
+        embed_url = data["embed_url"]
+        await ctx.respond(embed_url)
