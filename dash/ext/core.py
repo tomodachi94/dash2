9d9a8c02c8907239f9902b1774adea20e4a8419c
Tomodachi94 <tomodachi94@protonmail.com>
2024-07-26 13:15:06 -0700

    ext.core: localize

--- dash/ext/core.py
+++ dash/ext/core.py
@@ -5,10 +5,14 @@ loader = lightbulb.Loader()
 
 @loader.command
 class AboutCommand(
-    lightbulb.SlashCommand, name="about", description="Tells you about the bot."
+    lightbulb.SlashCommand,
+    name="core.command.about.name",
+    description="core.command.about.desc",
+    localize=True,
 ):
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
+        # FIXME: Translate response
         await ctx.respond(
             """
     Dash is a bot for the FTB Wiki's Discord.
